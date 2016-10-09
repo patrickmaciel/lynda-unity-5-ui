d74e946fbf92bb0a2acddeb3a514fc87f6319a04
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-08 23:09:38 -0300

    019 Working with text in scripts

---
+++ Assets/Scripts/TextTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextTest : MonoBehaviour
+{
+
+    public string message;
+    //public Text textInstance;
+    private Text textInstance;
+    
+	// Use this for initialization
+	void Start ()
+	{
+	    textInstance = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    textInstance.text = message;
+	}
+}
