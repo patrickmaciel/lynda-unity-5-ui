93227b0cb034590dff2d77d85c3f24bad5578bc9
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-11 20:15:43 -0300

    030 Controlling a slider in scripts

---
+++ Assets/Scripts/SliderTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderTest : MonoBehaviour
+{
+
+    public Slider sliderInstance;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    sliderInstance.minValue = 0;
+	    sliderInstance.maxValue = 100;
+	    sliderInstance.wholeNumbers = true;
+	    sliderInstance.value = 50;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnValueChanged(float value)
+    {
+        Debug.Log("New value " + value);
+    }
+}
