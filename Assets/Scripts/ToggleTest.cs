e5c47623a1a762f430419dac28b139dfeada3a28
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-10-13 21:50:15 -0300

    039 Working with toggle groups in scripts

---
+++ Assets/Scripts/ToggleTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Linq;
+
+public class ToggleTest : MonoBehaviour
+{
+    private ToggleGroup toggleGroupInstance;
+
+    public Toggle currentSelection
+    {
+        get { return toggleGroupInstance.ActiveToggles().FirstOrDefault(); }
+    }
+
+	// Use this for initialization
+	void Start ()
+	{
+	    toggleGroupInstance = GetComponent<ToggleGroup>();
+        Debug.Log("First selected " + currentSelection.name);
+
+        SelectToggle(2);
+	}
+
+    public void SelectToggle(int id)
+    {
+        var toggles = toggleGroupInstance.GetComponentsInChildren<Toggle>();
+        toggles[id].isOn = true;
+    }
+}
